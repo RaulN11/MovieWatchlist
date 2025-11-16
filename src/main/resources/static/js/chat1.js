88bcb47729eb4b60cb9ce364a266731c68233456
RaulN11 <raulnicula2004@gmail.com>
2025-11-17 00:03:11 +0200

    Working send message in the chat page

--- src/main/resources/static/js/chat1.js
+++ src/main/resources/static/js/chat1.js
@@ -16,14 +16,67 @@ connect();
 
 users.forEach(user=>{
     user.addEventListener("click",()=>{
+        loadChatHistory(currentSender, currentReceiver);
         placeholder.style.display="none";
         chatContent.style.display="flex";
         chatContent.style.flexDirection="column";
         users.forEach(u=>u.classList.remove("active"));
         user.classList.add("active");
         const name=user.querySelector(".small-name").textContent;
+        currentReceiver=name
         document.querySelector(".chatting-user-name").textContent=name;
         const profilePic=user.querySelector(".small-profile").src;
         document.querySelector(".chatting-user-pic").src=profilePic;
     })
 })
+function setupSendMessage(){
+    const sendButton=document.getElementById("send-button");
+    const messageField=document.querySelector(".message-field")
+    sendButton.addEventListener("click", ()=>{
+        const content=messageField.value.trim();
+        if(content === "" || !currentReceiver) return;
+        const chatMessage={
+            senderName:currentSender,
+            receiverName:currentReceiver,
+            content:content
+        };
+        displayIncomingMessage(chatMessage);
+        stompClient.send("/app/chat", {}, JSON.stringify(chatMessage));
+        messageField.value="";
+    });
+}
+setupSendMessage();
+
+function displayIncomingMessage(message){
+    if(message.senderName !== currentReceiver) return;
+    const chatTexts=document.querySelector(".chat-texts");
+    const msgDiv= document.createElement("div");
+    msgDiv.classList.add("message", "incoming-message");
+    msgDiv.textContent=message.content;
+    chatTexts.appendChild(msgDiv);
+    chatTexts.scrollTop=chatTexts.scrollHeight;
+}
+function displayOutgoingMessage(message){
+    const chatTexts=document.querySelector(".chat-texts");
+    const msgDiv=document.createElement("div");
+    msgDiv.classList.add("message", "outgoing-message");
+    msgDiv.textContent=message.content;
+    chatTexts.appendChild(msgDiv);
+    chatTexts.scrollTop=chatTexts.scrollHeight;
+}
+function loadChatHistory(sender, receiver){
+    fetch('/messages/${sender}/${receiver}')
+        .then(res=>res.json())
+        .then(messages=>{
+            const chatTexts=document.querySelector(".chat-texts")
+            chatTexts.innerHTML="";
+            messages.forEach(msg=>{
+                if(msg.senderName===sender){
+                    displayOutgoingMessage(msg);
+                }else {
+                    displayIncomingMessage(msg);
+                }
+            });
+        });
+}
+
